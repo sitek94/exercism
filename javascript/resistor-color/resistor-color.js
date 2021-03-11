06de9b57dfcb7a83efe4a5814d38ae31aa9c8adb
sitek94 <msitkowski94@gmail.com>
2021-03-11 09:45:14 +0100

    finish 3rd exercise

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export function colorCode(colorName) {
+  return COLORS.indexOf(colorName);
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
