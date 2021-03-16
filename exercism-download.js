144f03c0db78bef3b8b5cefab744d07e206a8bdb
sitek94 <msitkowski94@gmail.com>
2021-03-16 14:12:58 +0100

    feat: add util to automatically download new exercise

---
+++ exercism-download.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+// Each time you download an exercise, exercism also downloads configuration
+// files which (package.json, etc.)
+//
+// I don't need these files because I have all my configuration files in the
+// root directory.
+//
+// This tool automatically downloads an exercise from Exercism and then removes
+// unwanted files.
+
+const { execSync } = require("child_process");
+
+const arg = process.argv;
+
+// Exercise name - first argument
+const exercise = arg[2];
+// Track of the exercise - second argument
+const track = arg[3];
+
+// Download thee exercise using `exercism download`
+execSync(`exercism download --exercise=${exercise} --track=${track}`);
+
+const exercisePath = `${track}/${exercise}`;
+
+// Stage downloaded files - this step is required to make the git aware of new
+// directory, so it can perform next step
+execSync(`git add ${exercisePath}`);
+
+// Remove unwanted files
+execSync(`git clean -fXd -- ${exercisePath}/`);
+
+// Commit newly downloaded exercise
+execSync(
+  `git commit -m "Download new exercise: ${exercise} (${track})" ${exercisePath}`
+);
