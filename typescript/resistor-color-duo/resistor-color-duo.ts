6bb1525a2b014be72dfe9684bff6aff827e9f67b
sitek94 <msitkowski94@gmail.com>
2021-03-22 11:47:01 +0100

    Solve resistor color duo (ts) exercise

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,31 @@
 export class ResistorColor {
-  private colors: string[]
+  private colors: string[];
 
   constructor(colors: string[]) {
-    this.colors = colors
+    if (colors.length < 2) {
+      throw new Error("At least two colors need to be present");
+    }
+
+    this.colors = colors;
   }
-  value = (): number => 0
+  value = (): number =>
+    Number(
+      this.colors
+        .slice(0, 2)
+        .map(c => COLORS.indexOf(c))
+        .join("")
+    );
 }
+
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
