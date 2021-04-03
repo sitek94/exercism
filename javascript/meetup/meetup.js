37b520a50a2f0a407fcc9f3223fab059f531fec6
sitek94 <msitkowski94@gmail.com>
2021-04-03 18:14:17 +0200

    Solve meetup (js) exercise

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,8 +1,52 @@
-//
-// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export function meetup(year, month, descriptor, weekdayName) {
+  const date = new Date(year, month - 1);
+  const weekday = weekdays.indexOf(weekdayName);
 
-export const meetup = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+  switch (descriptor) {
+    case "teenth":
+      date.setDate(13);
+      break;
+
+    case "first":
+      date.setDate(1);
+      break;
+
+    case "second":
+      date.setDate(8);
+      break;
+
+    case "third":
+      date.setDate(15);
+      break;
+
+    case "fourth":
+      date.setDate(22);
+      break;
+
+    case "fifth":
+      date.setDate(29);
+      break;
+
+    case "last":
+      const daysInMonth = new Date(year, month, 0).getDate();
+      date.setDate(daysInMonth - 6);
+
+      break;
+  }
+
+  while (date.getDay() !== weekday) {
+    date.setDate(date.getDate() + 1);
+  }
+
+  return date;
+}
+
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
